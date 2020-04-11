7c66d9e31a8bedad4f302cf67f7b0797fdc67cfe
shankar <shankarsumar10@gmail.com>
2020-04-11 04:34:40 -0700

    update css

--- src/Component/Loginform.js
+++ src/Component/Loginform.js
@@ -7,13 +7,13 @@ class Loginform extends Component {
             <div className="login">
                 <Form  >
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label className="mt-5">Email address</Form.Label>
+                        <Form.Label className="mt-5"><h2>Email address</h2></Form.Label>
                         <Form.Control type="email" placeholder="Enter email" onChange={(e) => { this.saveemail(e) }} />
 
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label className="text"><h2>Password</h2></Form.Label>
                         <Form.Control type="password" placeholder="Password" onChange={(e) => { this.savepass(e) }} />
                     </Form.Group>
                     <Form.Group controlId="formBasicCheckbox">
--- src/Component/Register.js
+++ src/Component/Register.js
@@ -1,64 +1,81 @@
-import React,{Component} from "react"
-import { Modal, Button } from "react-bootstrap"
+import React, { Component } from "react";
+import { Modal, Button } from "react-bootstrap";
 
-class Register extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            show: false,
-            savename: "",
-            saveemail: "",
-            savePassword: []
-        }
-    }
-    handalclick=()=>{
-        this.setState({show:!this.state.show})
-    }
-    hideModal=()=>{
-        this.setState({show:false})
-    }
-    savename = (e) => {
-        this.setState({ savename: e.target.value })
-    }
-    saveemail = (e) => {
-        
-        this.setState({ saveemail: e.target.value })
-    }
-    savePassword=(e)=>{
-        this.setState({savePassword: e.target.value})
-    }
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      savename: "",
+      saveemail: "",
+      savePassword: [],
+    };
+  }
+  handalclick = () => {
+    this.setState({ show: !this.state.show });
+  };
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+  savename = (e) => {
+    this.setState({ savename: e.target.value });
+  };
+  saveemail = (e) => {
+    this.setState({ saveemail: e.target.value });
+  };
+  savePassword = (e) => {
+    this.setState({ savePassword: e.target.value });
+  };
 
-    render(){
-        return(
-            <div className="iamge">
-                
-                <button className="button" onClick={this.handalclick}><b> Sign In</b></button>
+  render() {
+    return (
+      <div className="iamge">
+        <button className="button" onClick={this.handalclick}>
+          <b> Sign In</b>
+        </button>
 
-                <Modal show={this.state.show} onHide={this.hideModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>CREATE ACCOUNT </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-
-                        Enter Name:
-                        <input className="form-control mt-2 mb-2" placeholder="Enter Name" onChange={(e) => { this.savename(e) }}></input>
-                        Enter Email:
-                        <input className="form-control mt-2" placeholder="Enter Email" onChange={(e) => { this.saveemail(e) }}></input>
-                        Password:
-                        <input className="form-control mt-2" placeholder="Enter password" onChange={(e) => { this.savePassword(e) }}></input>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        
-                        <Button variant="primary" onClick={this.saveprofile}>
-                            Sign Up
-                         </Button>
-                    </Modal.Footer>
-                   </Modal>
-
-                
+        <Modal show={this.state.show} onHide={this.hideModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>CREATE ACCOUNT </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Enter Name:
+            <input
+              className="form-control mt-2 mb-2"
+              placeholder="Enter Name"
+              onChange={(e) => {
+                this.savename(e);
+              }}
+            ></input>
+            Enter Email:
+            <input
+              className="form-control mt-2"
+              placeholder="Enter Email"
+              onChange={(e) => {
+                this.saveemail(e);
+              }}
+            ></input>
+            Password:
+            <input
+              className="form-control mt-2"
+              placeholder="Enter password"
+              onChange={(e) => {
+                this.savePassword(e);
+              }}
+            ></input>
+          </Modal.Body>
+          <Modal.Footer>
+            <div>
+              <Button variant="primary" onClick={this.savePassword}>
+                Sign Up
+              </Button>
+              
             </div>
-
-        )
-    }
+            
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 export default Register;
