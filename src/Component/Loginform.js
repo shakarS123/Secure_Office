1eae016c6e4b5f57a14d2d2b36240e38210bd55b
jaicks <jaick0507@gmail.com>
2020-04-11 13:20:00 +0530

    this is first commit by jaicks

---
+++ src/Component/Loginform.js
@@ -0,0 +1,11 @@
+import React, {Component } from 'react'
+
+class Loginform extends Component {
+    render() {
+        return (
+            <div>
+                Hey this is Loginform. hey bro..jsiwhdi d0ijjh
+            </div>
+        )
+    }
+}
